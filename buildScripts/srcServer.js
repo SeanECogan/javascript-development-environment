deb9c182635e06d8345fab142f07de4252857b51
Sean Cogan <sean.e.cogan@gmail.com>
2017-08-31 22:24:15 -0400

    Setup centralized repository for all API calls using Fetch. Add JSON Schema Faker and JSON Server for making mock API calls.

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,14 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+    res.json([
+        {"id": 1, "firstName": "Sean", "lastName": "Cogan", "email": "sean.cogan@gmail.com"},
+        {"id": 2, "firstName": "Cogan", "lastName": "Sean", "email": "cogan.sean@gmail.com"},
+        {"id": 3, "firstName": "Testy", "lastName": "Test", "email": "testy.test@gmail.com"}
+    ]);
+});
+
 app.listen(port, function(err) {
     if (err) {
         console.log(err);
